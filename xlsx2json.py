3536842f4cc46d04a6ecb029d93f1f3eab6d1538
dongdonggun <138856015@qq.com>
2015-05-11 11:30:28 +0800

    support chinese, fix windows env bug, fix bool type bug, fix sheet bug

--- xlsx2json.py
+++ xlsx2json.py
@@ -6,6 +6,9 @@ import datetime
 from openpyxl import load_workbook
 
 def parse_cell_value(value):
+    #布尔类型
+    if isinstance(value, bool):
+        return value
     if not value:
         return ''
     #int类型
@@ -45,44 +48,48 @@ def parse_cell_value(value):
             return float(value)
         return int(value)
     #string类型
-    return value
+    return value.encode('utf-8')
     
 def get_workbooks(dir_name = 'excel'):
     excel_path = os.path.join(os.getcwd(), dir_name)
-#     print os.listdir(excel_path)
+    # print os.listdir(excel_path)
     file_list = os.listdir(excel_path)
     if not file_list:
         print 'no excel file !'
         return
     workbooks = []
     for i in file_list:
-        file_path = os.path.join(excel_path, i)
-#         print file_path
-        wb = load_workbook(file_path)
-        workbooks.append(wb)
+        #windows环境下，存在正在使用的.xlsx文件，会在当前目录下生成~$开头的文件
+        #读取时，忽略此文件
+        if i.find('~$') == -1:
+            file_path = os.path.join(excel_path, i)
+            # print file_path
+            wb = load_workbook(file_path)
+            workbooks.append(wb)
     return workbooks
 
 def save_json(file_name, json_data, dir_name = 'json'):
     json_path = os.path.join(os.getcwd(), dir_name)
     file_path = os.path.join(json_path, file_name + '.json')
     with open(file_path, 'w') as f:
-        json.dump(json_data, f, indent = 4)
+        json.dump(json_data, f, ensure_ascii = False, indent = 4)
 
 def xlsx2json(head_row = 2):
     workbooks = get_workbooks()
     for wb in workbooks:
         for sheet in wb:
-#             print sheet.title
-            head = sheet.rows[head_row - 1]
-#             print head
-            json_list = []
-            for row in sheet.rows[head_row:]:
-                row_dic = {}
-                for head_cell, cell in zip(head, row):
-#                     print head_cell.value, cell.value, type(cell.value)
-                    row_dic[head_cell.value] = parse_cell_value(cell.value)
-                json_list.append(row_dic)
-            save_json(sheet.title, json_list)
+            # print sheet.title
+            if sheet.rows:
+                head = sheet.rows[head_row - 1]
+    #             print head
+                json_list = []
+                for row in sheet.rows[head_row:]:
+                    row_dic = {}
+                    for head_cell, cell in zip(head, row):
+                        # print head_cell.value, cell.value, type(cell.value)
+                        row_dic[head_cell.value] = parse_cell_value(cell.value)
+                    json_list.append(row_dic)
+                save_json(sheet.title, json_list)
 
 if __name__ == '__main__':
     head_row = 2
